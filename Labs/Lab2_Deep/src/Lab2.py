7464e8944b14741dbc046eeb74b6bbcf74af3be7
ferles <alexandros.ferles@nb-aferles.xing.hh>
2018-07-02 20:34:46 +0200

    Migrated work from enterprise github, working on the presentation notebook

--- Labs/Lab2_Deep/src/Lab2.py
+++ Labs/Lab2_Deep/src/Lab2.py
@@ -1,11 +1,14 @@
-### Imports
-
 import numpy as np
 from matplotlib import pyplot as plt
 from keras.utils import to_categorical as make_class_categorical
 
-### Load Batch
 def LoadBatch(filename):
+    """
+    Loads batch based on the given filename and produces the X, Y, and y arrays
+
+    :param filename: Path of the file
+    :return: X, Y and y arrays
+    """
     # borrowed from https://www.cs.toronto.edu/~kriz/cifar.html
     def unpickle(file):
         import pickle
@@ -30,120 +33,302 @@ def LoadBatch(filename):
 
     return X, Y, y
 
-### Initialize weights
-def initialize_weights(d, m, K, variance=0.01):
+def initialize_weights(d, m, K, std=0.001):
+    """
+    Initializes the weight and bias arrays for the 2 layers of the network
 
-    W1= np.random.normal(0, variance, size=(m, d) )
-    b1= np.random.normal(0, variance, size=(m, 1) )
+    :param d: Dimensionality of the input data
+    :param m: Number of nodes in the first layer
+    :param K: Number of different classes (K=10 for the CIFAR-10 dataset)
+    :param variance (optional): The variance of the normal distribution that will be used for the initialization of the weights
 
-    W2 = np.random.normal(0, variance, size=(K, m))
-    b2 = np.random.normal(0, variance, size=(K, 1))
+    :return: Weights and bias arrays for the first and second layer of the neural network
+    """
+
+    W1= np.random.normal(0, std, size=(m, d) )
+    b1= np.zeros(shape=(m,1))
+
+    W2 = np.random.normal(0, std, size=(K, m))
+    b2 = np.zeros(shape=(K,1))
 
     return W1, b1, W2, b2
 
-### ReLU
 def ReLU(x):
+    """
+    Rectified Linear Unit function
+
+    :param x: Input to the function
+
+    :return: Output of ReLU(x)
+    """
 
     return max(0,x)
 
-### Softmax
+
 def softmax(X, theta=1.0, axis=None):
-
-    # Softmax over numpy rows and columns, taking care for overflow cases
-    # Many thanks to https://nolanbconaway.github.io/blog/2017/softmax-numpy
-    # Usage: Softmax over rows-> axis =0, softmax over columns ->axis =1
-
     """
-    Compute the softmax of each element along an axis of X.
-    Parameters
-    ----------
-    X: ND-Array. Probably should be floats.
-    theta (optional): float parameter, used as a multiplier
-        prior to exponentiation. Default = 1.0
-    axis (optional): axis to compute values along. Default is the
-        first non-singleton axis.
-    Returns an array the same size as X. The result will sum to 1
-    along the specified axis.
+    Softmax over numpy rows and columns, taking care for overflow cases
+    Many thanks to https://nolanbconaway.github.io/blog/2017/softmax-numpy
+    Usage: Softmax over rows-> axis =0, softmax over columns ->axis =1
+
+    :param X: ND-Array. Probably should be floats.
+    :param theta: float parameter, used as a multiplier prior to exponentiation. Default = 1.0
+    :param axis (optional): axis to compute values along. Default is the first non-singleton axis.
+
+    :return: An array the same size as X. The result will sum to 1 along the specified axis
     """
 
     # make X at least 2d
-    y = np.atleast_2d(X)
+    y= np.atleast_2d(X)
 
     # find axis
     if axis is None:
-        axis = next(j[0] for j in enumerate(y.shape) if j[1] > 1)
+        axis= next(j[0] for j in enumerate(y.shape) if j[1] > 1)
 
     # multiply y against the theta parameter,
-    y = y * float(theta)
+    y= y * float(theta)
 
     # subtract the max for numerical stability
-    y = y - np.expand_dims(np.max(y, axis=axis), axis)
+    y= y - np.expand_dims(np.max(y, axis=axis), axis)
 
     # exponentiate y
-    y = np.exp(y)
+    y= np.exp(y)
 
     # take the sum along the specified axis
-    ax_sum = np.expand_dims(np.sum(y, axis=axis), axis)
+    ax_sum= np.expand_dims(np.sum(y, axis=axis), axis)
 
     # finally: divide elementwise
-    p = y / ax_sum
+    p= y / ax_sum
 
     # flatten if X was 1D
     if len(X.shape) == 1: p = p.flatten()
 
     return p
 
-### Evaluate Classifier
 def EvaluateClassifier(X, W1, b1, W2, b2):
+    """
+    Computes the Softmax output of the 2 layer network, based on input data X and trained weight and bias arrays
 
-    s1= np.dot(W1,X)+ b1
-    h=ReLU(s1)
-    s=np.dot(W2,h)+b2
+    :param X: Input data
+    :param W1: Weight array of the first layer
+    :param b1: Bias vector of the first layer
+    :param W2: Weight array of the second layer
+    :param b2: Bias vector of the second layer
+
+    :return: Softmax output of the trained network
+    """
+
+    s1= np.dot(W1,X) + b1
+    h= ReLU(s1)
+    s= np.dot(W2,h) + b2
     p= softmax(s, axis=1)
 
-    return p, h, s1, s
+    return p, h, s1
 
-### Predict classes
 def predictClasses(p):
+    """
+    Predicts classes based on the softmax output of the network
+
+    :param p: Softmax output of the network
+    :return: Predicted classes
+    """
 
     return np.argmax(p, axis=0)
 
-### Compute Gradients
-def ComputeGradients(X, Y, W1, b1, W2, b2, regularization_term):
+def ComputeAccuracy(X, y, W1, b1, W2, b2):
+    """
+    Computes the accuracy of the feed-forward 2-layer network
+
+    :param X: Input data
+    :param y: Labels of the ground truth
+    :param W1: Weight matrix of the first layer
+    :param b1: Bias vector of the first layer
+    :param W2: Weight matrix of the second layer
+    :param b2: Bias vector of the second layer
+
+    :return: Accuracy metric of the neural network.
+    """
+    p, _, _ = EvaluateClassifier(X=X, W1=W1, b1=b1, W2=W2, b2=b2)
+    predictions = predictClasses(p)
+
+    accuracy= np.sum(np.where(predictions-y==0, 1, 0))
+
+    return accuracy
+
+def ComputeCost(X, Y, W1, b1, W2, b2, regularization_term):
+
+    p, _, _ = EvaluateClassifier(X=X, W1=W1, b1=b1, W2=W2, b2=b2)
+
+    cross_entropy_loss = np.sum(-np.log(np.dot(Y.T, p)), axis=1) / float(X.shape[0])
+
+    weight_sum = np.power(W1,2).sum() + np.power(W2,2).sum()
+
+    return cross_entropy_loss + regularization_term*weight_sum
+
+def ComputeGradsNum(X, Y, W1, b1, W2, b2, regularization_term, h=1e-5):
+
+    """
+    Computes gradient descent updates on a batch of data with numerical computations.
+    Contributed by Josephine Sullivan for educational purposes for the DD2424 Deep Learning in Data Science course.
+
+    :param X: Input data
+    :param Y: One-hot representation of the true labels of input data X
+    :param W1: Weight matrix of the first layer
+    :param b1: Bias vector of the first layer
+    :param W2: Weight matrix of the second layer
+    :param b2: Bias vector of the second layer
+    :param regularization_term: Contribution of the regularization in the weight updates
+
+    :return: Weight and bias updates of the first and second layer of our network computed with numerical computations
+    """
+
+    grad_W1= np.zeros(W1.shape)
+    grad_b1= np.zeros(b1.shape)
+    grad_W2= np.zeros(W2.shape)
+    grad_b2= np.zeros(b2.shape)
+    
+    c = ComputeCost(X=X, Y=Y, W1=W1, b1=b1, W2=W2, b2=b2, regularization_term=regularization_term)
+    
+    for i in range(b1.shape[0]):
+        b1_try = b1[i,0]
+        b1_try[i,0] += h
+        c2 = ComputeCost(X=X, Y=Y, W1=W1, b1=b1_try, W2=W2, b2=b2, regularization_term=regularization_term)
+        grad_b1[i,0] = (c2-c) / h
+
+    for i in range(b2.shape[0]):
+        b2_try = b2[i,0]
+        b2_try[i,0] += h
+        c2 = ComputeCost(X=X, Y=Y, W1=W1, b1=b1, W2=W2, b2=b2_try, regularization_term=regularization_term)
+        grad_b2[i,0] = (c2-c) / h
+        
+    for i in range(W1.shape[0]):
+        for j in range(W1.shape[1]):
+            
+            W1_try= W1
+            W1_try[i,j] += h
+            c2= ComputeCost(X=X, Y=Y, W1=W1_try, b1=b1, W2=W2, regularization_term=regularization_term)
+            
+            grad_W1[i,j] = (c2-c) / h
+
+    for i in range(W2.shape[0]):
+        for j in range(W2.shape[1]):
+            W2_try = W2
+            W2_try[i, j] += h
+            c2 = ComputeCost(X=X, Y=Y, W1=W1, b1=b1, W2=W2_try, regularization_term=regularization_term)
+
+            grad_W2[i, j] = (c2 - c) / h
+
+    return W1, b1, W2, b2
+
+def ComputeGradsNumSlow(X, Y, W1, b1, W2, b2, regularization_term, h=1e-5):
+    """
+    Computes gradient descent updates on a batch of data with numerical computations of great precision, thus slower computations.
+    Contributed by Josephine Sullivan for educational purposes for the DD2424 Deep Learning in Data Science course.
+
+    :param X: Input data
+    :param Y: One-hot representation of the true labels of input data X
+    :param W1: Weight matrix of the first layer
+    :param b1: Bias vector of the first layer
+    :param W2: Weight matrix of the second layer
+    :param b2: Bias vector of the second layer
+    :param regularization_term: Contribution of the regularization in the weight updates
+
+    :return: Weight and bias updates of the first and second layer of our network computed with numerical computations with high precision.
+    """
 
     grad_W1= np.zeros(W1.shape)
     grad_b1= np.zeros(b1.shape)
     grad_W2= np.zeros(W2.shape)
     grad_b2= np.zeros(b2.shape)
 
-    p, h, s1, s = EvaluateClassifier(X, W1, b1, W2, b2)
-    for datum_index in range(X.shape[1]):
+    for i in range(b1.shape[0]):
 
-        g= (Y[:,datum_index] - p[:,datum_index]).T
+        b1_try = b1[i,0]
+        b1_try[i,0] -= h
+        c1 = ComputeCost(X=X, Y=Y, W1=W1, b1=b1_try, W2=W2, b2=b2, regularization_term=regularization_term)
 
-        #  Add gradient of l w.r.t. b2 & W2 computed at (x, y)
-        grad_b2+= g
-        grad_W2+= np.dot(g.T, h.T)
+        b1_try = b1[i,0]
+        b1_try[i,0] += h
+        c2 = ComputeCost(X=X, Y=Y, W1=W1, b1=b1_try, W2=W2, b2=b2, regularization_term=regularization_term)
+        grad_b1[i,0] = (c2-c1) / (2*h)
 
-        # Back-propagate gradient through 2nd fully connected layer
+    for i in range(b2.shape[0]):
+        b2_try = b2[i, 0]
+        b2_try[i, 0] -= h
+        c1 = ComputeCost(X=X, Y=Y, W1=W1, b1=b1, W2=W2, b2=b2_try, regularization_term=regularization_term)
 
-        g= np.dot(g, W2)
-        g= np.dot(g, np.diag(s1.clip(0)) )
+        b2_try = b2[i, 0]
+        b2_try[i, 0] += h
+        c2 = ComputeCost(X=X, Y=Y, W1=W1, b1=b1, W2=W2, b2=b2_try, regularization_term=regularization_term)
+        grad_b2[i, 0] = (c2 - c1) / (2 * h)
 
-        # Add gradient of l w.r.t.first layer parameters computed at(x, y)
-        grad_b1+= g
+    for i in range(W1.shape[0]):
+        for j in range(W1.shape[1]):
 
-        grad_W1+= np.dot(g.T, X[:,datum_index]).T
+            W1_try = W1
+            W1_try[i, j] -= h
+            c1 = ComputeCost(X=X, Y=Y, W1=W1_try, b1=b1, W2=W2, regularization_term=regularization_term)
 
-    grad_W1/=X.shape[1]
-    grad_W2/=X.shape[1]
-    grad_b1/=X.shape[1]
-    grad_b2/=X.shape[1]
+            W1_try = W1
+            W1_try[i, j] += h
+            c2 = ComputeCost(X=X, Y=Y, W1=W1_try, b1=b1, W2=W2, regularization_term=regularization_term)
 
-    grad_W1+= 2*regularization_term*W1
-    grad_W2+= 2*regularization_term*W2
+            grad_W1[i, j] = (c2 - c1) / (2 * h)
+
+    for i in range(W2.shape[0]):
+        for j in range(W2.shape[1]):
+
+            W2_try = W2
+            W2_try[i, j] -= h
+            c1 = ComputeCost(X=X, Y=Y, W1=W1, b1=b1, W2=W2_try, regularization_term=regularization_term)
+
+            W2_try = W2
+            W2_try[i, j] += h
+            c2 = ComputeCost(X=X, Y=Y, W1=W1, b1=b1, W2=W2_try, regularization_term=regularization_term)
+
+            grad_W2[i, j] = (c2 - c1) / (2 * h)
+
+    return W1, b1, W2, b2
+
+def ComputeGradients(X, Y, W1, b1, W2, b2, regularization_term):
+
+    """
+    Computes gradient descent updates on a batch of data
+
+    :param X: Input data
+    :param Y: One-hot representation of the true labels of input data X
+    :param W1: Weight matrix of the first layer
+    :param b1: Bias vector of the first layer
+    :param W2: Weight matrix of the second layer
+    :param b2: Bias vector of the second layer
+    :param regularization_term: Contribution of the regularization in the weight updates
+
+    :return: Weight and bias updates of the first and second layer of our network
+    """
+
+    # Evaluate the classifier to the batch
+    p, h, s1 = EvaluateClassifier(X=X, W1=W1, b1=b1, W2=W2, b2=b2)
+
+    # Back-propagate second layer at first
+
+    # Gradient of J w.r.t second bias vector is the g vector:
+    g = (Y-p).T
+    grad_b2= np.sum(g, axis=1).reshape(b2.shape[0],1) / X.shape[0]
+    # Gradient of J w.r.t second weight matrix is the matrix:
+    grad_W2 = np.dot(g.T, h.T) / 255 + 2 * regularization_term * W2
+
+    # Back-propagate the gradient vector g to the first layer
+    g= np.dot(g,W2) * np.where(s1 >0, 1, 0)
+
+    grad_b1= np.sum(g, axis=1).reshape(b1.shape[0], 1) / X.shape[0]
+    grad_W1= np.dot(g.T , X.T) / X.shape[0]
+
+    # Add regularizers
+    grad_W1+= 2 * regularization_term * W1
+    grad_W2+= 2 * regularization_term * W2
 
     return grad_W1, grad_b1, grad_W2, grad_b2
+
 
 
 
