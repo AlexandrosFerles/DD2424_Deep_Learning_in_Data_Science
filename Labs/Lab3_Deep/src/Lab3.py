48b6cf95aab288feed7bf527327e58131c0c14f6
ferles <ferles@kth.se>
2018-08-08 14:56:08 +0200

    Fixed some functions through debugging

--- Labs/Lab3_Deep/src/Lab3.py
+++ Labs/Lab3_Deep/src/Lab3.py
@@ -35,10 +35,6 @@ def LoadBatch(filename):
 
     return X, Y, y
 
-"""
-EXERCISE 1: Upgrade your code from assignment 2 so that you can train a k-layer network.
-"""
-
 def initialize_weights(shapes_list, std=0.001):
     """
     Initializes the weight and bias arrays for the 2 layers of the network
@@ -130,9 +126,14 @@ def EvaluateClassifier(X, weights, biases):
     intermediate_outputs = [] # s's
     intermediate_activations = [] #h's
 
-    for i in range(0, len(weights) -1 ):
+    s = np.dot(weights[0], X) + biases[0]
+    intermediate_outputs.append(s)
+    h = ReLU(s)
+    intermediate_activations.append(h)
 
-        s = np.dot(weights[i], X) + biases[i]
+    for i in range(1, len(weights) - 2):
+
+        s = np.dot(weights[i], intermediate_activations[-1]) + biases[i]
         intermediate_outputs.append(s)
         h = ReLU(s)
         intermediate_activations.append(h)
@@ -193,6 +194,66 @@ def ComputeCost(X, Y, weights, biases, regularization_term=0):
 
     return cross_entropy_loss + regularization_term * weight_sum
 
+def ComputeGradsNumSlow(X, Y, weights, biases, regularization_term=0, h=1e-5):
+    """
+    Computes gradient descent updates on a batch of data with numerical computations of great precision, thus slower computations.
+    Contributed by Josephine Sullivan for educational purposes for the DD2424 Deep Learning in Data Science course.
+
+    :param X: Input data.
+    :param Y: One-hot representation of the true labels of input data X.
+    :param weights: Weights arrays of the k layers.
+    :param biases: Bias vectors of the k layers.
+    :param regularization_term: Contribution of the regularization in the weight updates.
+
+    :return: Weight and bias updates of the k layers of our network computed with numerical computations with high precision.
+    """
+    
+    grad_weights = []
+    grad_biases = []
+    
+    for layer_index in range(len(weights)):
+        
+        W = weights[layer_index]
+        b = biases[layer_index]
+        
+        grad_W = np.zeros(W.shape)
+        grad_b = np.zeros(b.shape)
+
+        for i in tqdm(range(b.shape[0])):
+            b_try = np.copy(b)
+            b_try[i, 0] -= h
+            temp_biases = biases.copy()
+            temp_biases[layer_index] = b_try
+            c1 = ComputeCost(X=X, Y=Y, weights=weights, biases=temp_biases)
+            b_try = np.copy(b)
+            b_try[i, 0] += h
+            temp_biases = biases.copy()
+            temp_biases[layer_index] = b_try
+            c2 = ComputeCost(X=X, Y=Y, weights=weights, biases=temp_biases)
+
+            grad_b[i, 0] = (c2 - c1) / (2 * h)
+
+        grad_biases.append(grad_b)
+
+        for i in tqdm(range(W.shape[0])):
+            for j in range(W.shape[1]):
+                W_try = np.copy(W)
+                W_try[i, j] -= h
+                temp_weights = weights.copy()
+                temp_weights[layer_index] = W_try
+                c1 = ComputeCost(X=X, Y=Y, weights=temp_weights, biases=biases)
+                W_try = np.copy(W)
+                W_try[i, j] += h
+                temp_weights = weights.copy()
+                temp_weights[len(weights) + layer_index] = W_try
+                c2 = ComputeCost(X=X, Y=Y, weights=temp_weights, biases=biases)
+
+                grad_W[i, j] = (c2 - c1) / (2 * h)
+
+        grad_weights.append(grad_W)
+
+    return grad_weights, grad_biases
+
 def ComputeGradients(X, Y, weights, biases, p, outputs, activations, regularization_term=0):
     """
     Computes gradient descent updates on a batch of data
@@ -247,6 +308,42 @@ def ComputeGradients(X, Y, weights, biases, p, outputs, activations, regularizat
         weight_updates[index] += 2*regularization_term * weight_updates[index]
 
     return weight_updates, bias_updates
+
+def check_similarity(grad_weights, grad_biases, num_weights, num_biases):
+    """
+    Compares the gradients of both the analytical and numerical method and prints out a message of result
+    or failure, depending on how close these gradients are between each other.
+
+    :param grad_weights: Analytically computed gradients of the weights
+    :param grad_biases: Analytically computed gradients of the biases
+    :param num_weights: Numerically computed gradients of the weights
+    :param num_biases: Numerically computed gradients of the biases
+
+    :return: None
+    """
+
+    for layer_index in range(len(grad_weights)):
+
+        print('-----------------')
+        print(f'Layer no. {layer_index+1}:')
+
+        weight_abs = np.abs(grad_weights[layer_index] - num_weights[layer_index])
+        bias_abs = np.abs(grad_biases[layer_index] - num_biases[layer_index])
+
+        weight_nominator = np.average(weight_abs)
+        bias_nominator = np.average(bias_abs)
+
+        grad_weight_abs = np.absolute(grad_weights[layer_index])
+        grad_weight_num_abs = np.absolute(num_weights[layer_index])
+
+        grad_bias_abs = np.absolute(grad_biases[layer_index])
+        grad_bias_num_abs = np.absolute(num_biases[layer_index])
+
+        sum_weight = grad_weight_abs + grad_weight_num_abs
+        sum_bias = grad_bias_abs + grad_bias_num_abs
+
+        print(f'Deviation on weight matrix: {weight_nominator / np.amax(sum_weight)}')
+        print(f'Deviation on bias vector: {bias_nominator / np.amax(sum_bias)}')
 
 def initialize_momentum(arrays):
     """
@@ -328,7 +425,7 @@ def MiniBatchGDwithMomentum(X, Y, X_validation, Y_validation, y_validation, GDpa
 
     best_validation_set_accuracy = 0
 
-    for epoch in tqdm(range(epoches)):
+    for _ in tqdm(range(epoches)):
         # for epoch in range(epoches):
 
         for batch in range(1, int(X.shape[1] / number_of_mini_batches)):
@@ -365,6 +462,39 @@ def MiniBatchGDwithMomentum(X, Y, X_validation, Y_validation, y_validation, GDpa
 
 def exercise_1():
 
+    X_training_1, Y_training_1, y_training_1 = LoadBatch('../../cifar-10-batches-py/data_batch_1')
+    X_training_2, Y_training_2, y_training_2 = LoadBatch('../../cifar-10-batches-py/data_batch_2')
+    X_test, _, y_test = LoadBatch('../../cifar-10-batches-py/test_batch')
+
+    mean = np.mean(X_training_1)
+    X_training_1 -= mean
+    X_training_2 -= mean
+    X_test -= mean
+
+    # Check with numerically computed gradients for 2-layer network
+
+    # weights, biases = initialize_weights([[50, 3072], [10, 50]])
+    # W1_num = np.load('grad_W1_num.npy')
+    # W2_num = np.load('grad_W2_num.npy')
+    #
+    # b1_num = np.load('grad_b1_num.npy')
+    # b2_num = np.load('grad_b2_num.npy')
+    #
+    # num_weights = [W1_num, W2_num]
+    # num_biases = [b1_num, b2_num]
+    #
+    # p, activations, outputs = EvaluateClassifier(X_training_1[:, 0:2], weights, biases)
+    # grad_weights, grad_biases = ComputeGradients(X_training_1[:, 0:2], Y_training_1[:, 0:2], weights, biases, p, outputs, activations)
+    #
+    # check_similarity(grad_weights, grad_biases, num_weights, num_biases)
+
+    # Check with numerically computed gradients for 3-layer network
+
+    weights, biases = initialize_weights([[50, 3072], [20, 50], [10, 20]])
+
+    grad_weights_3_num, grad_bias_3_num = ComputeGradsNumSlow(X_training_1[:, 0:2], Y_training_1[:, 0:2], weights, biases)
+def exercise_2():
+
     # Test that you are able to replicate the results of a 2-layer network
 
     X_training_1, Y_training_1, y_training_1 = LoadBatch('../../cifar-10-batches-py/data_batch_1')
@@ -394,8 +524,6 @@ def exercise_1():
 
     validation_set_accuracy = ComputeAccuracy(X_training_2, y_training_2, weights, biases)
     print('Validation set accuracy of this setting: ', validation_set_accuracy)
-
-
 
 if __name__ =='__main__':
 
