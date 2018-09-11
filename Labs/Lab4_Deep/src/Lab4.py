63bee7906bc2e180e661d8eb42c500b07b5b5be1
ferles <ferles@kth.se>
2018-09-11 22:55:40 +0200

    Added softmax function

--- Labs/Lab4_Deep/src/Lab4.py
+++ Labs/Lab4_Deep/src/Lab4.py
@@ -41,6 +41,46 @@ def Ind_to_Char(integer_list, unique_chars_list):
 
     return [unique_chars_list[int(elem)] for elem in integer_list]
 
+def softmax(X, theta=1.0, axis=None):
+    """
+    Softmax over numpy rows and columns, taking care for overflow cases
+    Many thanks to https://nolanbconaway.github.io/blog/2017/softmax-numpy
+    Usage: Softmax over rows-> axis =0, softmax over columns ->axis =1
+
+    :param X: ND-Array. Probably should be floats.
+    :param theta: float parameter, used as a multiplier prior to exponentiation. Default = 1.0
+    :param axis (optional): axis to compute values along. Default is the first non-singleton axis.
+
+    :return: An array the same size as X. The result will sum to 1 along the specified axis
+    """
+
+    # make X at least 2d
+    y = np.atleast_2d(X)
+
+    # find axis
+    if axis is None:
+        axis = next(j[0] for j in enumerate(y.shape) if j[1] > 1)
+
+    # multiply y against the theta parameter,
+    y = y * float(theta)
+
+    # subtract the max for numerical stability
+    y = y - np.expand_dims(np.max(y, axis=axis), axis)
+
+    # exponentiate y
+    y = np.exp(y)
+
+    # take the sum along the specified axis
+    ax_sum = np.expand_dims(np.sum(y, axis=axis), axis)
+
+    # finally: divide elementwise
+    p = y / ax_sum
+
+    # flatten if X was 1D
+    if len(X.shape) == 1: p = p.flatten()
+
+    return p
+
 class RNN:
     """
     Recurrent Neural Network object
