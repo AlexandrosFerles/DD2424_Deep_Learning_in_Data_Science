05b0c973289dc14135115418eae61dec2502a724
ferles <ferles@kth.se>
2018-09-11 22:34:22 +0200

    Added RNN class

--- Labs/Lab4_Deep/src/Lab4.py
+++ Labs/Lab4_Deep/src/Lab4.py
@@ -41,6 +41,41 @@ def Ind_to_Char(integer_list, unique_chars_list):
 
     return [unique_chars_list[int(elem)] for elem in integer_list]
 
+class RNN:
+    """
+    Recurrent Neural Network object
+    """
+
+    def __init__(self, m=100, K=10, eta=0.1, seq_length=25, std=0.01):
+        """
+        Initial setting of the RNN.
+
+        :param m: Dimensionality of the hidden state.
+        :param K: Number of unique classes to identify.
+        :param eta: The learning rate of the training process.
+        :param seq_length: The length of the input sequence.
+        :param std: the variance of the normal distribution that initializes the weight matrices.
+        """
+
+        self.m = m
+        self.K = K
+        self.eta = eta
+        self.seq_length = seq_length
+        self.std = std
+
+    def init_weights(self):
+        """
+        Initializes the weights and bias matrices
+        """
+
+        U = np.random.randn(self.m, self.K) * self.std
+        W = np.random.randn(self.m, self.m) * self.std
+        U = np.random.randn(self.K, self.m) * self.std
+
+
+
+
+
 
 if __name__=='__main__':
     book_data, unique_characters = Load_Text_Data()
