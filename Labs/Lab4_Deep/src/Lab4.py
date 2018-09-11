98361d74b477ab4c15fc861cd46da5e4ecde0908
ferles <ferles@kth.se>
2018-09-11 22:00:13 +0200

    Adde functions for mapping integers to characters and characters to integers

--- Labs/Lab4_Deep/src/Lab4.py
+++ Labs/Lab4_Deep/src/Lab4.py
@@ -14,11 +14,39 @@ def Load_Text_Data(file_path='../goblet_book.txt'):
     :return: book_data: all input characters and unique_characters: unique single characters of the input data.
     """
     book_data = open(file_path, 'r').read()
-    unique_characters = set(book_data)
+    unique_characters = list(set(book_data))
 
     return book_data, unique_characters
+
+def Char_to_Ind(unique_chars):
+    """
+    Maps the original characters to integers.
+
+    :param unique_chars: The set of unique characters available.
+
+    :return: A list of integers taht correspond to the characters.
+    """
+
+    return [index for index, _ in enumerate(unique_chars)]
+
+def Ind_to_Char(integer_list, unique_chars_list):
+    """
+    Maps a list of integers to thei corresponding characters,
+
+    :param integer_list: A list of integer representation of a character sequence.
+    :param unique_chars_list: The list of unique characters.
+
+    :return: The actual character sequence.
+    """
+
+    return [unique_chars_list[int(elem)] for elem in integer_list]
 
 
 if __name__=='__main__':
     book_data, unique_characters = Load_Text_Data()
+
+    integers = Char_to_Ind(unique_characters)
+    lst = [2, 3, 5]
+    chars = Ind_to_Char(lst, unique_characters)
+
     main()
