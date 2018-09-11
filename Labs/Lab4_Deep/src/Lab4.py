f4b6eb20ed2febe5f52bbaf30e47913e7a360b4b
ferles <ferles@kth.se>
2018-09-11 21:48:08 +0200

    Added read data function

--- Labs/Lab4_Deep/src/Lab4.py
+++ Labs/Lab4_Deep/src/Lab4.py
@@ -5,5 +5,20 @@ def main():
 
     print('Finished!')
 
+def Load_Text_Data(file_path='../goblet_book.txt'):
+    """
+    Reads the input data.
+
+    :param file_path: (Optional) Position of the txt file in the local system.
+
+    :return: book_data: all input characters and unique_characters: unique single characters of the input data.
+    """
+    book_data = open(file_path, 'r').read()
+    unique_characters = set(book_data)
+
+    return book_data, unique_characters
+
+
 if __name__=='__main__':
+    book_data, unique_characters = Load_Text_Data()
     main()
